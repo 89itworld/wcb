a5ae6d6922ac89e4bb1aeee5f575320028f1ac57
Manoj Rathore <89itworld@gmail.com>
2016-07-29 18:51:17 +0530

    v1 of front end

--- app/Models/Retailers.php
+++ app/Models/Retailers.php
@@ -8,10 +8,23 @@ class Retailers extends Model
 {
     //
     protected $primaryKey = 'retailer_id';
+
+
+    /*
+   * Created At: 29-July-2016
+   *Author: Amit
+   *Description:Relation with network table
+  */
+    public  function Affnetwork(){
+        return $this->belongsTo('App\Models\Affnetwork','network_id','network_id');
+    }
+
+
     public function coupons()
     {
         return $this->hasMany('App\Models\Coupons','retailer_id');
     }
+    
     public function getCouponsPaginatedAttribute()
     {
         return $this->coupons()->paginate(5);
